8c59697ec5d31fa0eb6d3eb9f1df37d0d5b42418
SrDiego17 <diegomateo56@gmail.com>
2024-11-08 21:08:23 +0100

    Admin discoteca pages and add discoteca done

---
+++ proyecto/components/RatingBoxAdmin.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+
+const RatingBoxAdmin = ({ name, rating, discoteca, city }) => {
+  const router = useRouter();
+  const [showModal, setShowModal] = useState(false);
+  const [currentRating, setCurrentRating] = useState(rating);
+
+  useEffect(() => {
+    setCurrentRating(rating);
+  }, [rating]); // inicialmente, cambiamos el rating al que me da el usuario. luego, lo modificaremos por el nuevo calculado
+
+  const handleCardClick = () => {
+    router.push(`/discoteca/${discoteca}`);
+  };
+
+  const handleRatingClick = (e) => {
+    e.stopPropagation();
+    setShowModal(true);
+  };
+
+
+  const handleDatos = () => {
+    router.push('/admin/admin'); // Redirige a la página principal o a una lista de discotecas;
+  };
+  
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+ 
+  const submitRating = async (star) => {
+    try {
+      const response = await fetch(`/api/puntuar/${discoteca}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify({
+          ciudad: city,
+          rating: star,
+        }),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(`Puntuación enviada: ${star} estrellas`);
+        setCurrentRating(data.updatedRating); // Actualiza la puntuación promedio desde la respuesta
+        setShowModal(false);
+      } else {
+        console.error('Error al guardar la puntuación');
+      }
+    } catch (error) {
+      console.error('Error al enviar la puntuación:', error);
+    }
+  };
+
+  // Componente del modal
+  const RatingModal = () => (
+    <div className="modal-overlay" onClick={closeModal}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <h2>Puntuar {name}</h2>
+        <div className="rating-options">
+          {[1, 2, 3, 4, 5].map((star) => (
+            <button key={star} onClick={() => submitRating(star)}>
+              {star} ⭐
+            </button>
+          ))}
+        </div>
+        <button onClick={closeModal}>Cerrar</button>
+      </div>
+    </div>
+  );
+
+  return (
+    <>
+      <div className="card" onClick={handleCardClick}>
+        <div className="content">
+          <p className="heading">{name}</p>
+          <p className="heading">{city}</p>
+          <p className="heading" onClick={handleDatos}>Datos del local</p>
+          <p className="rating" onClick={handleRatingClick}>{currentRating}</p>
+        </div>
+      </div>
+      {showModal && <RatingModal />}
+    </>
+  );
+};
+
+export default RatingBoxAdmin;
---
+++ proyecto/pages/api/discotecas/gestor/[correo].js
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const { correo } = req.query; // `ciudad` comes from the dynamic route
+
+    try {
+      // Query the database, filter by city
+      const discotecas = await prisma.discoteca.findMany({
+        where: {
+          gestor: correo, // Filtrar por ciudad
+        },
+      });
+
+      // Close the Prisma connection
+      await prisma.$disconnect();
+      return res.status(200).json(discotecas);
+    } catch (error) {
+      console.error('Error fetching discotecas:', error);
+      return res.status(500).json({ error: 'Failed to fetch discotecas' });
+    } finally {
+      await prisma.$disconnect;
+    }
+  } else {
+    // If it's not a GET request
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+}
